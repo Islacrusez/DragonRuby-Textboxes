fc46dfb417552aed83aa2bae1c389df06080b046
Islacrusez || m4rek <m4rek@hotmail.com>
2021-02-20 10:52:15 +0000

    Added comments to main.rb
    All parts of code are now commented.

--- app/main.rb
+++ app/main.rb
@@ -43,43 +43,44 @@ def textbox(string, x, y, w, size=0, font="default")
 	end
 end
 
-def get_length(string, size=0, font="default")
+def get_length(string, size=0, font="default") # Internal method utilising calcstringbox to return string box length
 	$gtk.args.gtk.calcstringbox(string, size, font).x
 end
 
-def get_height(string, size=0, font="default")
+def get_height(string, size=0, font="default") # Internal method utilising calcstringbox to return string box height
 	$gtk.args.gtk.calcstringbox(string, size, font).y
 end
 
 def string_to_lines(string, box_x, size, font)
 	return string unless get_length(string, size, font) > box_x
-	string.gsub!("\r", '')
-	strings_with_linebreaks = string.split("\n")
+	string.gsub!("\r", '')                                      # Removes carriage returns, leaving only line breaks
+	strings_with_linebreaks = string.split("\n")                # splits string into array at linebreak
 	list_of_strings = strings_with_linebreaks.map do |line| 
-		next if line == ""
-		line.split
+		next if line == ""										# Ignores blank strings, as caused by consecutive linebreaks
+		line.split												# Splits strings into arrays of words at any whitespace
+																# Results in nested array, [[],[]]!
 	end
 	list_to_lines(list_of_strings, box_x, size, font)
 end
 
 def list_to_lines(strings, box_x, size, font)
-	line = ""
-	lines = []
-	strings.map!{|string| string << ""}.flatten!.pop
+	line = ""													# Define string
+	lines = []													# Define array
+	strings.map!{|string| string << ""}.flatten!.pop			# Adds a blank 'word' to the end of each outer array, to trigger newline code
 	strings.each do |word|
-		if word.empty? || !word
-			lines.push line.dup unless line.empty?
-			lines.push " " if line.empty?
-			line.clear
-		elsif get_length(line + " " + word, size, font) <= box_x
-			line << " " if line.length > 0
-			line << word
-		else
-			lines.push line.dup
-			line.clear
-			line << word
+		if word.empty? || !word									# Handling of blank 'words' and Nil entries in arrays 
+			lines.push line.dup unless line.empty?				# Adds existing accumulated words to the current line
+			lines.push " " if line.empty?						# Adds a space if no words accrued
+			line.clear											# Clears the accumulator
+		elsif get_length(line + " " + word, size, font) <= box_x	# "If current word fits on the end of the current line, do"
+			line << " " if line.length > 0						# Inserts a space into accumulator if the line isn't blank
+			line << word										# Adds the current word to the accumulator
+		else														# "If the word doesn't fit, instead do"
+			lines.push line.dup									# Adds accumulator to current line
+			line.clear											# Clears accumulator
+			line << word										# Adds current word to accumulator
 		end
-	end
-	lines.push line.dup
-	lines
+	end															# Once all words in all strings are processed
+	lines.push line.dup											# Add accumulator to current line, as it's possible for accumulator to not have been committed
+	return lines														# Return array of lines, explicitly to be safe.
 end
