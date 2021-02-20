394ed22684f191af7f3ba36bfb1d85360d67a6c1
Islacrusez || m4rek <m4rek@hotmail.com>
2021-02-20 16:02:32 +0000

    Update main.rb
    Minor tweaks

--- app/main.rb
+++ app/main.rb
@@ -16,22 +16,23 @@ textbox, get_length, get_height, string_to_lines, list_to_lines
 Usage:
 
 Copy or require the code into your project, and call
-Minimum:
-args.outputs.labels << textbox(\"Text\", x, y, w)
 
+Minimum:
+
+args.outputs.labels << textbox(\"Text\", x, y, w)
 where \"Text\" is the string you wish to turn into a textbox
 x and y are the xy pixel coordinate of the top left corner of your textbox
 and w is the width, in pixels, of the textbox you wish to create.
 
 All supported:
-args.outputs.labels << textbox(\"Text\", x, y, w, size, font)
 
+args.outputs.labels << textbox(\"Text\", x, y, w, size, font)
 In line with the .labels output and args.gtk.calcstringbox, textbox supports:
 size (as a positive or negative integer, with default 0)
 font (a string of the font name, default \"default\")
 Logic suggestions, troubleshooting and other support has been kindly lent to this project by the DragonRuby Discord community. They're an awesome bunch and you should come visit sometime. We look forward to seeing your creations!"
 
-    args.state.box_size ||= [20, 700, 200]  # change this and restart DR to change target textbox size and location
+    args.state.box_size ||= [20, 700, 400]  # change this and restart DR to change target textbox size and location
 
     args.state.text_box ||= textbox(args.state.text, *args.state.box_size, args.state.text_size, args.state.text_font)
     args.state.text_height ||= get_height(args.state.text, args.state.text_size, args.state.text_font) * args.state.text_box.length
